2d67276fa05248f98295571c4b123e5d74c07c26
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-02-20 21:46:43 +0530

    react

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <h1>Heloo About</h1>
+      
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        fugiat ab velit. Aspernatur culpa, voluptatem velit esse ab optio
+        facilis, a expedita rerum ex tenetur perspiciatis dicta, et repellat
+        harum!
+      </p>
+    </>
+  );
+}
+
+export default About;
